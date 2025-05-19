ff3e65cf851fcda09f1b4fe04d385bcfc03abaeb
Benjamin Decker <39363928+BenjaminDecker@users.noreply.github.com>
2025-05-19 10:30:01 +0200

    Update other.py

--- src/quark/interface_types/other.py
+++ src/quark/interface_types/other.py
@@ -9,7 +9,7 @@ class Other:
     """Represents data that does not conform to any of the other interface types provided by quark.
 
     If possible, use any of the other datatypes in your module's pre- and postprocessing methods. This will increase the
-    that other modules can be used alongside it without needing any intermediate type-conversion modules.
+    probability that other modules can be used alongside it without needing any intermediate type-conversion modules.
     """
 
     data: Any
