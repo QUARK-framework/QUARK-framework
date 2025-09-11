71d88ca2f660b021fae113ff4c04af02baad4018
Daniel Porawski <d.porawski@gmx.de>
2025-09-11 12:42:09 +0200

    lp interface type

---
+++ src/quark/interface_types/lp.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+
+class LP:
+    """A class for representing linear programs (LP) readable by common LP solvers like CPLEX and SCIPYOPT."""
+
+    _lp_string: str
+
+    @classmethod
+    def from_str(cls, string: str) -> LP:
+        """Create the interface type from an LP-string."""
+        lp = cls()
+        lp._lp_string = string
+
+        return lp
+
+    @classmethod
+    def from_file(cls, file_path: str | Path) -> LP:
+        """Create a LP from a file."""
+        lp = cls()
+
+        with Path(file_path).open("r") as file:
+            lp._lp_string = file.read()
+
+        return lp
